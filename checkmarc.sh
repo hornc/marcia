1a89424875dec34283496eea3cec8ab0fcf6818f
Charles Horn <charles.horn@gmail.com>
2018-01-16 15:15:39 +1300

    tidy output text

--- checkmarc.sh
+++ checkmarc.sh
@@ -10,8 +10,8 @@ clr="\e[0m"
 # Check Ids without MARC XML if <itemlist> provided
 if [ $1 ] && [ -e $1 ]; then
   missing_marc=$(comm -23 <(sort $1) <(ls *_marc.xml | sed -n 's/_marc.xml//p' | sort))
-  echo -e "\n$red!!! The following $(count $missing_marc) items did not have MARC XML.$clr"
-  echo "     Writing to 'no_marc.txt'"
+  echo -e "\n$red!!! $(count $missing_marc) items did not have MARC XML.$clr"
+  echo "     Writing list to 'no_marc.txt'"
   echo $missing_marc | tr " " "\n" > no_marc.txt
 elif [ $1 ]; then
   echo -e "\n$red!!! Itemlist '$1' not found!$clr"
@@ -26,8 +26,8 @@ egrep --color $bad_unicode *_marc.xml
 
 if [ $? -eq 0 ]; then
   bad_marc8_list=$(egrep -l $bad_unicode *_marc.xml)
-  echo -e "\n$red!!! The following $(count $bad_marc8_list) MARCs have potential MARC8 -> Unicode issues:$clr"
-  echo "     Writing to 'bad_unicode.txt'"
+  echo -e "\n$red!!! $(count $bad_marc8_list) MARCs have potential MARC8 -> Unicode issues:$clr"
+  echo "     Writing list to 'bad_unicode.txt'"
   echo $bad_marc8_list | tr " " "\n" > bad_unicode.txt
 
   echo -e "\n${yellow}Try re-converting from raw MARC with Yaz using '-f marc8 -t utf8' options.$clr"
@@ -38,8 +38,8 @@ fi
 bad_index=$(egrep -l "at end of field length=40" *_marc.xml)
 
 if [ $(count $bad_index) -ne 0 ]; then
-  echo -e "\n$red!!! The following $(count $bad_index) MARCs had a corrupt source index:$clr"
-  echo "     Writing to 'bad_index.txt'"
+  echo -e "\n$red!!! $(count $bad_index) MARCs had a corrupt source index:$clr"
+  echo "     Writing list to 'bad_index.txt'"
   echo $bad_index | tr " " "\n" > bad_index.txt
 fi
 
@@ -49,8 +49,8 @@ fi
 other_xml_comments=$(egrep -l '\-\->' /dev/null $(grep -L "at end of field length=40" *_marc.xml))
 
 if [ $(count $other_xml_comments) -ne 0 ]; then
-  echo -e "\n$yellow!!! The following $(count $other_xml_comments) MARCs contain other XML comments, which indicates conversion problems:$clr"
-  echo "     Writing to 'xml_comments.txt'"
+  echo -e "\n$yellow!!! $(count $other_xml_comments) MARCs contain other XML comments, which indicates conversion problems:$clr"
+  echo "     Writing list to 'xml_comments.txt'"
   echo $other_xml_comments | tr " " "\n" > xml_comments.txt
 fi
 
