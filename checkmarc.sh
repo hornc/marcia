3f84b89123c1c30e65586ac16d529de9bcef5167
Charles Horn <charles.horn@gmail.com>
2018-03-02 11:05:07 +1300

    expand comment as Arabic and Cyrillic are also matched

--- checkmarc.sh
+++ checkmarc.sh
@@ -30,7 +30,7 @@ bad_unicode+="|â[AeE]"  # > acute + vowel likely to be an encoding error
 bad_unicode+="|á[AE]"   # > grave + vowel likely to be an encoding error
 bad_unicode+="|ðc"      # > ç
 bad_unicode+="|¶"       # > œ
-bad_unicode+="|\(B[^a-z)]{,2}<" # unconverted Chinese MARC8 charset
+bad_unicode+="|\(B[^a-z)]{,2}<" # unconverted non-Latin MARC8 charsets in 880 fields
 
 egrep --color $bad_unicode *_marc.xml
 
