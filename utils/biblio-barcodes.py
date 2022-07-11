5f864caffa705e23b036fe3afd6ba6243d97270b
Charles Horn <charles.horn@gmail.com>
2022-07-11 02:18:41 +0000

    Improve description. Suppress debug output.

--- utils/biblio-barcodes.py
+++ utils/biblio-barcodes.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 """
-Takes a binary MARC file containg bibliographic records (possibly mixed with holdings records)
-and adds a custom / local filed 976$a field for local barcode for importing.
+Takes a binary MARC file containing bibliographic records (possibly mixed with holdings records)
+and adds a custom / local field 976$a for local barcode importing.
 """
 
 import sys
 from pymarc import Field, MARCReader, MARCWriter
 
+
 fnbiblio = sys.argv[1]
 fnbarcode = sys.argv[2]
+
 
 # Load .tsv of 001 ids to local barcodes
 with open(fnbarcode, 'r') as bc:
@@ -21,9 +23,11 @@ with open(fnbarcode, 'r') as bc:
         else:
             barcodes[id_] = [barcode]
 
+
 m = 150  # max records to process (for DEBUG)
 DEBUG = False
 writer = None
+
 
 # Load bibliographic MARC records to add barcodes
 # Output written to file out.mrc
@@ -32,17 +36,17 @@ with open(fnbiblio, 'rb') as marcdata:
     records = MARCReader(marcdata, to_unicode=True)
     for i, record in enumerate(records):
         if record is None:
-            print('None record')
+            print('Empty record')
             #continue
         if record['876']:
-            #print('HOLDING found!')
+            print('HOLDING found!')
             continue
         else:
-            print('%s Biblio found' % i)
+            #print('%s Biblio found' % i)
             enc = record.leader[9]
-            if enc != ' ':
-                 print('%s non MARC8 record found!' % i)
-            
+            #if enc != ' ':
+            #     print('%s non MARC8 record found!' % i)
+
             id_ = record['001'].value().strip()
             barcode = barcodes.get(id_)
             if barcode:
